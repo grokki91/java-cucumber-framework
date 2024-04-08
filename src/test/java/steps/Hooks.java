bf858bbfb8e1a9d599ecbdb7c215fbaaa23eaa19
Pavel Chekalin <grokki91@gmail.com>
2024-04-08 23:38:18 +0300

    Create screenshot folder. Native example

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -5,7 +5,11 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
 import settings.WebDriverSettings;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Hooks {
     @Before
@@ -14,10 +18,17 @@ public class Hooks {
     }
 
     @After
-    public static void tearDown(Scenario scenario) {
+    public static void tearDown(Scenario scenario) throws IOException {
         if (scenario.isFailed()) {
-            final byte[] screenshot = ((TakesScreenshot) WebDriverSettings.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName());
+            File srcFile = ((TakesScreenshot) WebDriverSettings.getDriver()).getScreenshotAs(OutputType.FILE);
+            String useDir = System.getProperty("user.dir");
+            File directory = new File(useDir, "screenshots");
+
+            if (!directory.exists()) {
+                directory.mkdir();
+                FileHandler.copy(srcFile, new File("./screenshots/" + scenario.getName() + ".png"));
+
+            }
         }
 
         WebDriverSettings.tearDown();
